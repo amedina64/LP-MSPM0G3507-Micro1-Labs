ae9d80279a0a7a8ee6ede6af51c1420ca744bbc6
Alejandro Medina <77434784+amedina64@users.noreply.github.com>
2025-02-03 03:07:35 -0700

    Added useful comments

--- GPIO/Blink.c
+++ GPIO/Blink.c
@@ -1,8 +1,6 @@
 #include "ti/devices/msp/m0p/mspm0g350x.h"
 #include "ti/driverlib/dl_common.h"
 
-#define OUTPUT 0x81
-
 int main(void)
 {
     // Step 1: Reset ports (EXACTLY ONCE), power ports and wait at least 24 clk cycles
@@ -14,15 +12,16 @@ int main(void)
 
     // Step 2: Select mode for each pin you will be using
     // In this example, we will use PA0 as a simple output pin
-
-    IOMUX->SECCFG.PINCM[0] = OUTPUT;
+    // Index 0 of PINCM is associated with PA0
+    IOMUX->SECCFG.PINCM[0] = 0x81; 
     GPIOA->DOE31_0 |= 1;
 
     while (1) 
     {
+        // LED01 toggles every second. Default clock frequency is 32 MHz
         GPIOA->DOUT31_0 |= 0x01;
-        DL_Common_delayCycles(3000000);
-        GPIOA->DOUT31_0 &= ~(0x01);
-        DL_Common_delayCycles(3000000);
+        DL_Common_delayCycles(32000000);
+        GPIOA->DOUT31_0 &= ~0x01;
+        DL_Common_delayCycles(32000000);
     }
 }
